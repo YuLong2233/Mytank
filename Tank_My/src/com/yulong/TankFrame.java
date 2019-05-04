e1e332de39506994d3f6beb62992e6554c0818ac
YuLong2233 <5840157879@qq.com>
2019-05-04 17:08:23 +0800

    重写Frame类里的paint方法，画出矩形。

--- Tank_My/src/com/yulong/TankFrame.java
+++ Tank_My/src/com/yulong/TankFrame.java
@@ -1,18 +1,21 @@
 package com.yulong;
 
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class TankFrame {
-	public static void main(String[] args) {
-		Frame f = new Frame();
-		f.setSize(500, 500);
-		f.setResizable(false);
-		f.setTitle("Tank War");
-		f.setVisible(true);
+public class TankFrame extends Frame {
 		
-		f.addWindowListener(new WindowAdapter() {
+		//重写TankFrame的构造方法，让TankFram类自己构造时生成窗口
+		TankFrame(){
+		Frame f = new Frame();
+		this.setSize(500, 500);
+		this.setResizable(false);
+		this.setTitle("Tank War");
+		this.setVisible(true);
+		
+		this.addWindowListener(new WindowAdapter() {
 
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -21,5 +24,10 @@ public class TankFrame {
 			
 			
 		});
-	}
+	}	
+		//重写Frame类里的paint方法，画出矩形图像
+		@Override
+		public void paint(Graphics g) {
+			g.fillRect(100, 100, 50, 40);
+		}
 }	
---
+++ Tank_My/src/com/yulong/main.java
@@ -0,0 +1,11 @@
+package com.yulong;
+
+public class main {
+
+	public static void main(String[] args) {
+		//在主方法里new一个TankFrame;
+		TankFrame tankframe = new TankFrame();
+
+	}
+
+}
